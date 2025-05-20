2a96b227ee130089e96ef381cff4b031818c4ee5
mybamed <bang18sad@gmail.com>
2025-05-21 00:45:59 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,46 @@
+
+package com.ptbabelmedia.mybamedwebapp
+
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        progressBar = findViewById(R.id.progressBar)
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progressBar.visibility = View.VISIBLE
+                progressBar.progress = newProgress
+                if (newProgress == 100) {
+                    progressBar.visibility = View.GONE
+                }
+            }
+        }
+
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://mybamed.web.app")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
